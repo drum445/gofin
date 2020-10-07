189e77aabc224a19806db75bdaa821b36f69b471
Alessandro Rinaldi <ale.rinaldi@criptalia.it>
2020-10-07 18:53:15 +0200

    Add rate iterations limit

--- rate_test.go
+++ rate_test.go
@@ -11,3 +11,10 @@ func TestRate(t *testing.T) {
 		t.Errorf("Sum was incorrect, got: %v, want: %v.", rate, rateExpected)
 	}
 }
+
+func TestRateWithLimit(t *testing.T) {
+	_, err := RateWithLimit(14, 3.54017, 44.4057, 0, 0, 100)
+	if err == nil {
+		t.Errorf("limit was not triggered")
+	}
+}
